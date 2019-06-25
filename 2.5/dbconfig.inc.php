5784422f40c1500ad91d43d8262c7e7a44661d53
gid9798 <30450294+gid9798@users.noreply.github.com>
2019-06-25 03:34:07 +0300

    add port an timezone

--- 2.5/dbconfig.inc.php
+++ 2.5/dbconfig.inc.php
@@ -1,7 +1,7 @@
 <?php
     define("DB_NAME", getenv('OCS_DBNAME'));
-    define("SERVER_READ", getenv('OCS_DBSERVER_READ'));
-    define("SERVER_WRITE", getenv('OCS_DBSERVER_WRITE'));
+    define("SERVER_READ", getenv('OCS_DBSERVER_READ').':'.getenv('OCS_DBSERVER_READ_PORT'));
+    define("SERVER_WRITE", getenv('OCS_DBSERVER_WRITE').':'.getenv('OCS_DBSERVER_WRITE_PORT'));
     define("COMPTE_BASE", getenv('OCS_DBUSER'));
     define("PSWD_BASE", getenv('OCS_DBPASS'));
     $_SESSION["PSWD_BASE"]=PSWD_BASE;
