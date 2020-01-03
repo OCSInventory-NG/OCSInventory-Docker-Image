fde830b6fcdede7a639a123fc119c5578155fc6b
Gilles Dubois <gilles.dubois@factorfx.com>
2020-01-03 12:50:32 +0100

    Structure / refactor and fix minor bug

--- 2.6/scripts/docker-entrypoint.sh
+++ 2.6/scripts/docker-entrypoint.sh
@@ -50,9 +50,9 @@ sed -i 's/PATH_TO_SNMP_DIR/'"${OCS_VARLIB_DIR//\//\\/}"'snmp/g' /etc/httpd/conf.
 
 # Generate dbconfig.inc.php
 cp /tmp/dbconfig.inc.php $OCS_WEBCONSOLE_DIR
-sed -i 's/OCS_DB_NAME/'"$OCS_DB_SERVER"'/g' $OCS_WEBCONSOLE_DIR/dbconfig.inc.php
-sed -i 's/OCS_READ_NAME/'"$OCS_DB_NAME"'/g' $OCS_WEBCONSOLE_DIR/dbconfig.inc.php
-sed -i 's/OCS_WRITE_NAME/'"$OCS_DB_NAME"'/g' $OCS_WEBCONSOLE_DIR/dbconfig.inc.php
+sed -i 's/OCS_DB_NAME/'"$OCS_DB_NAME"'/g' $OCS_WEBCONSOLE_DIR/dbconfig.inc.php
+sed -i 's/OCS_READ_NAME/'"$OCS_DB_SERVER"'/g' $OCS_WEBCONSOLE_DIR/dbconfig.inc.php
+sed -i 's/OCS_WRITE_NAME/'"$OCS_DB_SERVER"'/g' $OCS_WEBCONSOLE_DIR/dbconfig.inc.php
 sed -i 's/OCS_DB_USER/'"$OCS_DB_USER"'/g' $OCS_WEBCONSOLE_DIR/dbconfig.inc.php
 sed -i 's/OCS_DB_PASS/'"$OCS_DB_PASS"'/g' $OCS_WEBCONSOLE_DIR/dbconfig.inc.php
 
